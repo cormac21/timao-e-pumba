582ff0447794fe72a3d2facebea9c27dd822d240
cormacx <gaio.lucas0@gmail.com>
2022-08-15 15:41:51 -0300

    Commit inicial;

---
+++ src/main/java/com/cormacx/timaoepumba/TimaoEPumbaApplication.java
@@ -0,0 +1,13 @@
+package com.cormacx.timaoepumba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimaoEPumbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimaoEPumbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cormacx/timaoepumba/TimaoEPumbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cormacx.timaoepumba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimaoEPumbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
